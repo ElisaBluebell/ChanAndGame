ee97e0a70aad03fc1f44e05c146d7af0e620f211
ElisaBluebell <selfishracer@hotmail.com>
2023-01-30 20:26:40 +0900

    230130 ElisaBluebell

--- chat_client.py
+++ chat_client.py
@@ -1,9 +1,28 @@
+
+import sys
+
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 room_ui = uic.loadUiType('room.ui')[0]
+qt_ui = uic.loadUiType('main.ui')[0]
+
+
+class MainWindow(QMainWindow, qt_ui):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.chat_client = ChatClient()
 
 
 class ChatClient(QMainWindow, room_ui):
     def __init__(self):
         super().__init__()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    app.exec()
+
