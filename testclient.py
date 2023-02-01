7d2d83771cafb77fc1018182dc3e00ed01b6271b
jungyeonwoo265 <oqpwe7@naver.com>
2023-02-01 11:33:46 +0900

    test

--- testclient.py
+++ testclient.py
@@ -16,27 +16,36 @@ class WindowClass(QMainWindow, form_class):
 
         self.initialize_socket(sip, sport)
 
-        # self.make_room.clicked.connect(self.roommake)
+        self.make_room.clicked.connect(self.roommake)
         self.set_nickname.clicked.connect(self.nickmake)
 
+    # 서버에 통신 연결 및 닉네임 확인
     def initialize_socket(self, sip, sport):
         self.c = socket(AF_INET, SOCK_STREAM)
         self.c.connect((sip, sport))
         r_msg = self.c.recv(1024)
         if r_msg:
-            self.nickname.setText(r_msg.decode())
+            self.nickname.setText(f'{r_msg.decode()}님 환영합니다.')
 
+    # 방만들기
     def roommake(self):
         print('아직')
 
+    # 닉네임 설정 하기
     def nickmake(self):
-        nick = self.nickname_input.text()
-        if nick:
-            self.c.send(nick.encode())
-            r_msg = self.c.recv(1024)
-            if r_msg.decode() == 'True':
-                self.nickname.setText(f'{nick}님 환영합니다.')
-                self.nickname_input.clear()
+        self.c.send('닉네임'.encode())
+        r_msg = self.c.recv(1024)
+        if r_msg.decode() == 'True':
+            nick = self.nickname_input.text()
+            if nick:
+                self.c.send(nick.encode())
+                r_msg = self.c.recv(1024)
+                if r_msg.decode() == 'True':
+                    self.nickname.setText(f'{nick}님 환영합니다.')
+                    self.nickname_input.clear()
+                else:
+                    QMessageBox.information(self, '안내창', '닉네임이 중복되었습니다.')
+                    self.nickname_input.clear()
 
 
 if __name__ == "__main__":
